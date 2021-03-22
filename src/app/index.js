35fc52c25d18e5955679de500287576189ed0ee5
Spixs <info@iizii.in>
2021-03-22 14:57:20 +0530

    create first  routing in react

--- src/app/index.js
+++ src/app/index.js
@@ -2,44 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Header } from "../components/Header";
 import { Home } from "../components/Home";
+import { Root } from "./components/Root";
+import {User} from "./component/User";
+import { Route, Router } from "react-router";
 
 class App extends React.Component {
-  
-  constructor(){
-    super();
-    this.state = {
-      homeLink: "Home"
-    }
-  }
-
-  
-  onChangeLinkName(newName) {
-   this.setState({homeLink: newName})
-  }
-
-  onGreet() {
-   alert('from child to parent component cummunication.')
-  }
-
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <Header homeLink={this.state.homeLink}/>
-        </div>
-        <div className="row">
-          <div className="col-xs-10 col-xs-offset-1">
-            <Home 
-            name={"pawan"}
-            initialAge={25}
-            greet={this.onGreet}
-            changeLink={this.onChangeLinkName.bind(this)}
-            homeLink={this.state.homeLink}
-            />
-          </div>
-        </div>
-      </div>
-    );
+    <Router>
+     <Route path={"user"} component={User} />
+     <Route path={"home"} component={Home} />
+    </Router>
+    )
   }
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,88 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Home = () => {
+  
+  return (
+    <div className="container">
+      <h1>Home!</h1>
+    </div>
+  )
 
-export class Home extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      age: props.initialAge,
-      homeLink: props.homeLink,
-      status: 0,
-    }
-    setTimeout(() => {
-      this.setState({status: 1})
-    }, 3000);
-  }
-
-  onMakeOlder() {
-    this.setState({
-      age: (this.state.age += 3),
-    });
-  }
-
-  UNSAFE_componentWillMount() {
-    console.log("component will mount.")
-  }
-   
-  UNSAFE_componentDidMount() {
-    console.log("component did mount.")
-  }
-
-  UNSAFE_componentWillReciveProps(nextProps) {
-    console.log("component will recive props",nextProps)
-  }
-
-  shouldComponentUpdate(nextProps,nextState){
-    console.log("should component update",nextProps,nextState)
-    return true;
-  }
-
-  UNSAFE_componentWillUpdate(nextProps,nextState) {
-    console.log("component will update",nextProps,nextState)
-
-  }
-  componentDidUpdate(prevProps,prevState) {
-    console.log("component did update", prevProps, prevState)
-  }
-
-
-  onChangeName() {
-    this.props.changeLink(this.state.homeLink)
-  }
-
-  onChangeHandler(event) {
-    this.setState({homeLink: event.target.value});
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <p>This is Home components.</p>
-        <p> My name is {this.props.name} ,my age is {this.state.age}</p>
-        <p> Staus: {this.state.status}</p>
-         <div>
-          <button onClick={() => this.onMakeOlder()} className="btn btn-primary">
-           Make me older!
-          </button>
-          <hr/>
-          <button onClick={this.props.greet} className="btn btn-primary" >Greet</button>
-          <hr/>
-          <input type="text" 
-          value={this.state.homeLink} 
-          onChange={(event) => {this.onChangeHandler(event)}}/>
-          <button onClick={this.onChangeName.bind(this)} className="btn btn-primary">ChangeLink</button>     
-        </div>
-        <hr />
-        {this.props.children}
-      </div>
-    );
-  }
-}
-Home.propTypes = {}
-
-// Home.PropTypes = {
-//   name: React.PropTypes.string,
-//   initialAge: React.PropTypes.number,
-//   greet: React.PropTypes.func
-// }
+};
